eadf86313537b5a7015d03d1ab323edfaf3391d3
funakoshiapi <silvakoshis@gmail.com>
2020-08-05 17:06:06 -0400

    changes to card js excript

--- cards.js
+++ cards.js
@@ -19,12 +19,15 @@ function swap(swapEvent) {
     
     oldParag = document.getElementById("paragraph");
     oldParag.innerHTML = "Is a card game in which all of the cards are laid face down on a surface and two cards are flipped face up over each turn. The object of the game is to turn over pairs of matching cards.";
+    document.getElementById("proj--link").href="#";
   }
 
   //liker
   else if (value === '3'){
     oldParag = document.getElementById("paragraph");
     oldParag.innerHTML = "Chrome application that automate instagram likes, the application likes photos on the behalf of your account, pick the hashtags to follow and get to work";
+
+    document.getElementById("proj--link").href="#";
 
   }
 
@@ -33,6 +36,7 @@ function swap(swapEvent) {
     oldParag = document.getElementById("paragraph");
     oldParag.innerHTML = "Uses API's to render information about covid-19 cases in diferent countries. Itagrates graphical plots of the renderd data, helping to make sence of the information";
 
+    document.getElementById("proj--link").href="#";
 
   }
 
@@ -45,18 +49,22 @@ function swap(swapEvent) {
     oldParag = document.getElementById("paragraph");
     oldParag.innerHTML = "C++ desktop application that simulates an aquarium. The fish in the aqurium navigates the screen size on horizontal and vertical axis simulating an alive fish. The application menu allows picking one or mutiple fish. The menu also allows the user to add aquarium decoration to personalize the aquarium. ";
 
+    document.getElementById("proj--link").href="https://github.com/funakoshiapi/Object-Oriented---Software-Dev/tree/master/Aquarium";
+
 
   }
   else if (value === '5'){
     oldParag = document.getElementById("paragraph");
     oldParag.innerHTML = "Data manipulation of data file of city and county information in Michigan";
 
+    document.getElementById("proj--link").href="https://github.com/funakoshiapi/Projects/blob/master/DataManipulation.py";
 
   }
   else{
     oldParag = document.getElementById("paragraph");
     oldParag.innerHTML = "Designed graphical user interface, and programmed logic of winch to adjust the release of cabled sensor underwater based on desired depth. Wrote unit test to prevent system short circuit, that could be caused by realys connected to winch by microcontroller and portable power source.";
 
+    document.getElementById("proj--link").href="https://github.com/funakoshiapi/automated-winch";
   }
 
   if (swapEvent.target !== card) return;
